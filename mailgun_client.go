d18c135337304c8a219efc7855ce903f8be8dbec
christophberger <christophberger@users.noreply.github.com>
2025-06-30 22:36:13 +0200

    chore: merge MailgunClient into mailgun_client.go

--- mailgun_client.go
+++ mailgun_client.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -17,6 +20,12 @@ type EmailMessage struct {
 	Text           string
 	HTML           string
 	AttachmentPath string
+}
+
+// MIMEMessage is a helper struct for passing the
+type MIMEMessage struct {
+	message     bytes.Buffer
+	contentType string
 }
 
 // HTTPStatusError represents an HTTP status code and the related
@@ -88,4 +97,52 @@ func (c *MailgunClient) SendRequest(ctx context.Context, msg MIMEMessage) (id st
 
 	return response.Id, nil
 
+}
+
+func (c *MailgunClient) SendEmail(ctx context.Context, msg EmailMessage) (id string, err error) {
+	mime := MIMEMessage{}
+	writer := multipart.NewWriter(&mime.message)
+
+	writer.WriteField("from", msg.From)
+	writer.WriteField("subject", msg.Subject)
+
+	for _, recipient := range msg.To {
+		writer.WriteField("to", recipient)
+	}
+
+	if msg.Text != "" {
+		writer.WriteField("text", msg.Text)
+	}
+	if msg.HTML != "" {
+		writer.WriteField("html", msg.HTML)
+	}
+
+	if msg.AttachmentPath != "" {
+		file, err := os.Open(msg.AttachmentPath)
+		if err != nil {
+			return "", fmt.Errorf("failed to open attachment: %w", err)
+		}
+		defer file.Close()
+
+		fileWriter, err := writer.CreateFormFile("attachment", file.Name())
+		if err != nil {
+			return "", fmt.Errorf("failed to create form file: %w", err)
+		}
+
+		if _, err := io.Copy(fileWriter, file); err != nil {
+			return "", fmt.Errorf("failed to copy file: %w", err)
+		}
+
+	}
+
+	mime.contentType = writer.FormDataContentType()
+
+	writer.Close()
+
+	id, err = c.SendRequest(ctx, mime)
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
 }
